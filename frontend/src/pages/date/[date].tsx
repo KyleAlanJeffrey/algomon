eb8d49244880816b857fbf59c504923354f4c507
KyleAlanJeffrey <theawesomeguynextdoor@gmail.com>
2024-08-08 11:07:34 -0700

    feat: Add date specific page'

---
+++ frontend/src/pages/date/[date].tsx
@@ -0,0 +1,79 @@
+import { useQuery } from "@tanstack/react-query";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { fetchVideosByDateString } from "~/api";
+import WordCloud from "~/components/word-cloud";
+import { Video } from "~/types";
+
+export default function Home() {
+  const router = useRouter();
+  const videos = useQuery({
+    enabled: !!router.query.date, // only fetch if date is present
+    queryKey: ["videos"],
+    queryFn: async () => {
+      const response = await fetchVideosByDateString(
+        router.query.date as string,
+      );
+      if (response.status !== 200) {
+        throw new Error("Failed to fetch videos");
+      }
+      if (!response.data) {
+        throw new Error("No videos found");
+      }
+      return response.data as Video[];
+    },
+  });
+  return (
+    <>
+      <Head>
+        <title>Algomon</title>
+        <meta
+          name="description"
+          content="Algomon. The Youtube Algorithm Analyzer"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-start bg-themefireenginered">
+        <h1 className="font-extrabold tracking-tight text-white sm:text-[3rem]">
+          Youtube Algorithm
+        </h1>
+        <section
+          title="Todays Suggestions"
+          className="flex max-h-72 w-full flex-col items-center justify-start overflow-y-auto bg-themelapislazuli px-4"
+        >
+          <h2 className="font-semibold text-themebabypowdder sm:text-2xl">
+            Todays Recommended
+          </h2>
+          <ul className="gap-2">
+            {videos.data?.map((video) => (
+              <li key={video.url}>
+                <Link href={video.url}>
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={video.imageUrl}
+                      alt={video.title}
+                      className="h-24 w-24 rounded-lg object-cover"
+                    />
+                    <h2 className="text-l text-white">{video.title}</h2>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section
+          title="Todays Word Cloud"
+          className="flex w-full flex-col items-center justify-start bg-themeschoolbusyellow"
+        >
+          <h2 className="text-themelapislazul font-semibold sm:text-2xl">
+            Todays Word Cloud
+          </h2>
+          <WordCloud videos={videos.data} />
+        </section>
+      </main>
+    </>
+  );
+}
+
+// px-4 py-16
