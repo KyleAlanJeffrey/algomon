2852752df0e52e08296427570d75582cbe65fb14
KyleAlanJeffrey <theawesomeguynextdoor@gmail.com>
2024-08-14 12:55:56 -0700

    feat: add https for api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import fs from 'fs';
 async function bootstrap() {
   console.log('Starting server...');
   console.log(`Database URL: ${process.env.DATABASE_URL}`);
-  const app = await NestFactory.create(AppModule);
+
+  const keyFile = fs.readFileSync(
+    __dirname + '/etc/letsencrypt/live/algomon.kyle-jeffrey.com/privkey.pem',
+  );
+  const certFile = fs.readFileSync(
+    __dirname + '/etc/letsencrypt/live/algomon.kyle-jeffrey.com/fullchain.pem',
+  );
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    },
+  });
   app.enableCors();
   await app.listen(3001);
 }
